af2a9dabba8ca137658f74a265f9b0c5e100f2d8
George <dcel567@gmail.com>
2022-11-19 22:26:19 +0300

    untracked game state machine files added

---
+++ src/PlayState.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "PlayState.hpp"
+
+
+const std::string PlayState::s_playID = "PLAY";
+
+void PlayState::update()
+{
+	//nothing for now
+}
+
+void PlayState::render()
+{
+	//nothing for now
+}
+
+bool PlayState::onEnter()
+{
+	std::cout << "entering PlayState\n";
+	return true;
+}
+
+bool PlayState::onExit()
+{
+	std::cout << "exiting PlayState\n";
+	return true;
+}
